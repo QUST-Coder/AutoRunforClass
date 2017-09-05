7e9f5633c98ef950de040f0f62ed725275acc27f
CoderiGenius <755717181@qq.com>
2017-09-05 19:21:08 +0800

    Update 源码.js
    修复1.2版本中无法跳过章节测试和讨论的bug

--- 源码.js
+++ 源码.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.2.1
 // @description  try to take over the world!
 // @author       You
 // @match        *://qust.class.gaoxiaobang.com/class/*
@@ -10,11 +10,13 @@
 
 (function() {
     'use strict';
+
 function start(){
-	//加速
+//加速
 var vid = document.getElementById("vjs_video_3_html5_api");
+   if(vid!=undefined)
 vid.playbackRate = 2.5;
-	var classFix = document.getElementsByClassName("gxb-icon-check unchecked");
+		var classFix = document.getElementsByClassName("gxb-icon-check unchecked");
 	//勾选全部答案
 for(var i=0;i<classFix.length;i++){
 	var Fix = classFix[i];
@@ -36,6 +38,7 @@ for(var i=0;i<oBtn2.length;i++){
 
 //检测并跳过章节测试
 	var testCon = document.getElementsByClassName("gxb-btn-pri gxb-btn-big quiz-join chapter_id");
+     var testCon2 =  document.getElementsByClassName("gxb-btn-pri gxb-btn-big disable");
 	var continueBtn = document.getElementsByClassName("gxb-next-blue");
 	for(var r=0;r<testCon.length;r++)
 	{
@@ -45,8 +48,17 @@ for(var i=0;i<oBtn2.length;i++){
 		clickContinue.click();
 		}
 	}
+       for(var u=0;u<testCon2.length;u++)
+	{
+		var test2 = testCon2[u];
+		if(test2.className=="gxb-btn-pri gxb-btn-big disable"){
+		var clickContinue2 = continueBtn[u];
+		clickContinue2.click();
+		}
+    }
 	//检测并跳过讨论
 	var dicCon = document.getElementsByClassName("gxb-btn-pri gxb-btn-nav post-submit");
+    var dicCon2 = document.getElementsByClassName("gxb-btn-pri gxb-btn-nav post-submit-edit");
 	var continueBtn = document.getElementsByClassName("gxb-next-blue");
 	for(var f=0;f<dicCon.length;f++)
 	{
@@ -56,8 +68,16 @@ for(var i=0;i<oBtn2.length;i++){
 		clickContinue.click();
 		}
 	}
+    for(var f=0;f<dicCon.length;f++)
+	{
+		var test = dicCon[f];
+		if(test.className=="gxb-btn-pri gxb-btn-nav post-submit-edit"){
+		var clickContinue = continueBtn[f];
+		clickContinue.click();
+		}
+	}
 }
-window.setInterval(start,5000);
+window.setInterval(start,3000);
 
     // Your code here...
 })();
